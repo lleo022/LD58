f164fa207dd3b2a1512ea84a685ed453ebeb1a61
JoshVarela360 <jnvarel1@uci.edu>
2025-10-05 20:59:56 -0700

    All UI for title and settings completed
    Buttons all work and music pref is saved, just need to change music

---
+++ Assets/Scripts/Settings/SoundManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider _sliderVolume;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("MusicLevel"))
+        {
+            PlayerPrefs.GetFloat("MusicLevel", 1);
+            LoadMusic();
+
+        }
+        else
+        {
+            LoadMusic();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void ChangeVolume()
+    {
+        AudioListener.volume = _sliderVolume.value;
+        SaveVolume();
+    }
+    void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("MusicLevel", _sliderVolume.value);
+
+    }
+    void LoadMusic()
+    {
+       _sliderVolume.value = PlayerPrefs.GetFloat("MusicLevel");
+    }
+}
