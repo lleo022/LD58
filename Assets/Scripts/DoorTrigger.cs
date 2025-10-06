664ef627864f68b90807617559c8be1eef09e7f9
rqhu2025 <rhustudy@gmail.com>
2025-10-05 22:24:24 -0700

    made door animate
    also slightly tweaked intro 3 design

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -5,10 +5,12 @@ public class DoorTrigger : MonoBehaviour
     public int ones, twos, threes; //door open requirements
 
     private GameObject tracker;
+    private Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         tracker = GameObject.Find("Game Manager");
+        anim = transform.parent.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,12 +23,13 @@ public class DoorTrigger : MonoBehaviour
             if (Input.GetKey("e")) {
                 if (tracker.GetComponent<TrackCoins>().CanUseCoins(ones, twos, threes)) {
                     tracker.SendMessage("UseCoins", new int[] {ones, twos, threes});
+                    anim.SetTrigger("Door Opened");
                     RemoveDoor();
                 }
             }
         }
     }
     void RemoveDoor() {
-        Destroy(transform.parent.gameObject);
+        Destroy(transform.parent.gameObject, 0.9f);
     }
 }
